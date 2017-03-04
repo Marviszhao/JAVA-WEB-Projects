46e18cf5212954f9511054dbaa6de29a81275d6d
marvis_win <zhaozhifei668@163.com>
2017-03-04 13:05:07 +0800

    rest风格上传文件功能

---
+++ RestMyibatisSpring/src/com/rest/action/FileController_Rest.java
@@ -0,0 +1,41 @@
+package com.rest.action;
+
+import java.io.File;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+@Controller
+@RequestMapping("/file")
+public class FileController_Rest implements ServletContextAware{
+private ServletContext servletContext;
+	
+	@Override
+	public void setServletContext(ServletContext arg0) {
+		this.servletContext = arg0;
+	}
+	
+	@RequestMapping(value="/upload",method = RequestMethod.POST)
+	public String uploadFile(String fileName,@RequestParam("file")CommonsMultipartFile file) {
+		if (!file.isEmpty()) {
+			String path = this.servletContext.getRealPath("/upload");
+			String oriFileName = file.getOriginalFilename();
+			
+			String fileType = oriFileName.substring(oriFileName.lastIndexOf("."));
+			String newFileName = fileName + fileType;
+			File newFile = new File(path + "/" + newFileName);
+			try {
+				file.getFileItem().write(newFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return "/jsps/upload";
+	}
+}
